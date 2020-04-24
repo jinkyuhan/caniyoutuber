5f0c12a6f197bcd2b4d3f6f2fde3889e022f2696
jinkyuhan <gkswlsrb95@gmail.com>
2020-04-24 10:55:18 +0900

    feat: downloader_image_based_on done.

--- script/download_images_based_on_data.py
+++ script/download_images_based_on_data.py
@@ -2,17 +2,17 @@ import pandas
 import google_image_crawler
 
 
-DEBUG = True
+DEBUG = False
 
 file_names = ['subscriber_500_upper', 'subscriber_300_upper',
         'subscriber_100_upper', 'subscriber_50_upper', 'subscriber_30_upper', 'subscriber_10_upper']
+if DEBUG:
+    file_names = ['subscriber_500_upper']
 many_csv_data = []
-keywords = ""
 for file_name in file_names:
-    keywords = []
     csv_data = pandas.read_csv("../data/"+file_name+".csv", header=None)
-    if DEBUG == True:
+    if DEBUG:
         print(f"[{file_name}]: {len(csv_data)}")
     for row_idx in range(len(csv_data)):
-        keywords.append(f', {csv_data[1][row_idx]} 유튜브')
-    print(f"[{file_name}] : {keywords}")
+        channel_name = str(csv_data[1][row_idx])
+        google_image_crawler.download_google_staticimages('./chromedriver', f'{channel_name} 유튜브', f'/Volumes/Data/{file_name}/{channel_name}/', 300)
--- script/google_image_crawler.py
+++ script/google_image_crawler.py
@@ -11,7 +11,7 @@ import requests
 import urllib
 import urllib3
 from urllib3.exceptions import InsecureRequestWarning
-
+import pathlib
 import datetime
 import time
 
@@ -19,19 +19,27 @@ def download_google_staticimages(driver_path, keyword, dir_path, limit):
 
     urllib3.disable_warnings(InsecureRequestWarning)
 
+    t0 = time.time()
+    wating_time = 0.2
     searchword = keyword
     searchurl = 'https://www.google.com/search?q=' + searchword + '&source=lnms&tbm=isch'
     dirs = dir_path 
     maxcount = limit
 
     chromedriver = driver_path
+    try:
+        pathlib.Path(dirs).mkdir(parents=True, exist_ok=True)
+    except Exception as e:
+        print(f"Can't access dir_path: {e}")
+        sys.exit()
 
-    if not os.path.exists(dirs):
-        os.mkdir(dirs)
+    
+    # if not os.path.exists(dirs):
+    #     os.mkdir(dirs)
 
     options = webdriver.ChromeOptions()
     options.add_argument('--no-sandbox')
-    options.add_argument('--headless')
+    # options.add_argument('--headless')
 
     try:
         browser = webdriver.Chrome(chromedriver, options=options)
@@ -41,108 +49,116 @@ def download_google_staticimages(driver_path, keyword, dir_path, limit):
         sys.exit()
 
     browser.set_window_size(1280, 1024)
-    browser.get(searchurl)
-    time.sleep(1)
-
-    print(f'Getting you a lot of images. This may take a few moments...')
-
-    element = browser.find_element_by_tag_name('body')
-
-    # Scroll down
-    #for i in range(30):
-    for i in range(50):
-        element.send_keys(Keys.PAGE_DOWN)
-        time.sleep(0.3)
-
     try:
-        browser.find_element_by_id('smb').click()
-        for i in range(50):
-            element.send_keys(Keys.PAGE_DOWN)
-            time.sleep(0.3)
-    except:
-        for i in range(10):
-            element.send_keys(Keys.PAGE_DOWN)
-            time.sleep(0.3)
+        browser.get(searchurl)
+        browser.implicitly_wait(10)
 
-    print(f'Reached end of page.')
-    time.sleep(0.5)
-    print(f'Retry')
-    time.sleep(0.5)
+        print(f'Getting you a lot of images. This may take a few moments...')
 
-    # Below is in japanese "show more result" sentences. Change this word to your lanaguage if you require.
-    browser.find_element_by_xpath('//input[@value="결과 더보기"]').click()
+        element = browser.find_element_by_tag_name('body')
 
-    # Scroll down 2
-    for i in range(50):
-        element.send_keys(Keys.PAGE_DOWN)
-        time.sleep(0.3)
+        # Scroll down
+        #for i in range(30):
+        for _ in range(5):
+            browser.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+            browser.implicitly_wait(10)
+            # element.send_keys(Keys.PAGE_DOWN)
+            time.sleep(wating_time)
 
-    try:
-        browser.find_element_by_id('smb').click()
-        for i in range(50):
-            element.send_keys(Keys.PAGE_DOWN)
-            time.sleep(0.3)
-    except:
-        for i in range(10):
-            element.send_keys(Keys.PAGE_DOWN)
-            time.sleep(0.3)
-
-    #elements = browser.find_elements_by_xpath('//div[@id="islrg"]')
-    #page_source = elements[0].get_attribute('innerHTML')
-    page_source = browser.page_source 
-
-    soup = BeautifulSoup(page_source, 'html.parser')
-    images = soup.find_all('img')
-
-    urls = []
-    for image in images:
         try:
-            url = image['data-src']
-            if not url.find('https://'):
-                urls.append(url)
+            browser.find_element_by_id('smb').click()
+            for _ in range(50):
+                browser.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+                browser.implicitly_wait(10)
+                # element.send_keys(Keys.PAGE_DOWN)
+                time.sleep(wating_time)
         except:
+            for _ in range(10):
+                # element.send_keys(Keys.PAGE_DOWN)
+                browser.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+                browser.implicitly_wait(10)                
+                time.sleep(wating_time)
+
+        # Below is in japanese "show more result" sentences. Change this word to your lanaguage if you require.
+        browser.find_element_by_xpath('//input[@value="결과 더보기"]').click()
+        browser.implicitly_wait(10)
+        # Scroll down 2
+        for _ in range(5):
+            browser.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+            browser.implicitly_wait(10)
+            # element.send_keys(Keys.PAGE_DOWN)
+            time.sleep(wating_time)
+
+        try:
+            browser.find_element_by_id('smb').click()
+            for _ in range(5):
+                browser.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+                browser.implicitly_wait(10)
+                # element.send_keys(Keys.PAGE_DOWN)
+                time.sleep(wating_time)
+        except:
+            for _ in range(5):
+                browser.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+                browser.implicitly_wait(10)
+                # element.send_keys(Keys.PAGE_DOWN)
+                time.sleep(wating_time)
+
+        print('Page load done...')
+        print('Start to download...')
+
+        #elements = browser.find_elements_by_xpath('//div[@id="islrg"]')
+        #page_source = elements[0].get_attribute('innerHTML')
+        page_source = browser.page_source 
+
+        soup = BeautifulSoup(page_source, 'html.parser')
+        images = soup.find_all('img')
+
+        urls = []
+        for image in images:
             try:
-                url = image['src']
+                url = image['data-src']
                 if not url.find('https://'):
-                    urls.append(image['src'])
-            except Exception as e:
-                print(f'No found image sources.')
-                print(e)
+                    urls.append(url)
+            except:
+                try:
+                    url = image['src']
+                    if not url.find('https://'):
+                        urls.append(image['src'])
+                except Exception as e:
+                    print(f'No found image sources.')
+                    print(e)
 
-    count = 0
-    if urls:
-        for url in urls:
-            try:
-                res = requests.get(url, verify=False, stream=True)
-                rawdata = res.raw.read()
-                with open(os.path.join(dirs, 'img_' + str(count) + '.jpg'), 'wb') as f:
-                    f.write(rawdata)
-                    count += 1
-            except Exception as e:
-                print('Failed to write rawdata.')
-                print(e)
+        count = 0
+        if urls:
+            for url in urls:
+                if count >= maxcount:
+                    break;
+                try:
+                    res = requests.get(url, verify=False, stream=True)
+                    rawdata = res.raw.read()
+                    with open(os.path.join(dirs, 'img_' + str(count) + '.jpg'), 'wb') as f:
+                        print(f'Downloading ...{keyword} - [{dir_path}/img_{count}]')
+                        f.write(rawdata)
+                        count += 1
+                except Exception as e:
+                    print('Failed to write rawdata.')
+                    print(e)
+    finally:
+        browser.quit()
 
-    browser.close()
-    return count
+        t1 = time.time()
+
+        total_time = t1 - t0
+        print(f'\n')
+        print(f'Download completed. [Successful count = {count}].')
+        print(f'Total time is {str(total_time)} seconds.')
 
 # Main block
 def main(argv):
-    t0 = time.time()
+    
     if argv[1] == "--help":
         print('usage: python3 google_image_crawler [keyword] [dir_path] [limit]')
-    count = download_google_staticimages(argv[1], argv[2], argv[3], argv[4])
-    
-    t1 = time.time()
-
-    total_time = t1 - t0
-    print(f'\n')
-    print(f'Download completed. [Successful count = {count}].')
-    print(f'Total time is {str(total_time)} seconds.')
+    download_google_staticimages(argv[1], argv[2], argv[3], argv[4])
 
 if __name__ == '__main__':
     main(sys.argv)
-
-
-    
-    
-    
